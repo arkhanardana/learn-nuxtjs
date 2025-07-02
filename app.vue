92196f6611d073e28580c1ce5b2fc0a11d9a2af8
arkhanardana <hanzmantap@gmail.com>
2025-07-02 13:56:43 +0700

    Refactor app.vue to simplify layout structure and ensure Toaster styles are imported

--- app.vue
+++ app.vue
@@ -1,14 +1,13 @@
 <script setup lang="ts">
 import Navbar from "./components/navbar.vue";
 import { Toaster } from "./components/ui/sonner";
+import "vue-sonner/style.css";
 </script>
 
 <template>
   <Toaster />
   <NuxtLayout>
-    <header>
-      <Navbar />
-    </header>
+    <Navbar />
     <NuxtPage />
   </NuxtLayout>
 </template>
