b106f95f8211d03f02e4d8a8ed84272e5d7ba0ff
arkhanardana <hanzmantap@gmail.com>
2025-07-04 13:35:33 +0700

    feat: add helper for formattig date

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,14 @@
-import { type ClassValue, clsx } from 'clsx'
-import { twMerge } from 'tailwind-merge'
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+export const formatDate = (date: string | Date) => {
+  return new Intl.DateTimeFormat("id-ID", {
+    day: "numeric",
+    month: "long",
+    year: "numeric",
+  }).format(new Date(date));
+};
