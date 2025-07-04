28c26436200c8f4feca211c6d03ae82cc4981caf
arkhanardana <hanzmantap@gmail.com>
2025-07-04 13:38:10 +0700

    feat: add path /posts in protected routes list

--- middleware/auth.global.ts
+++ middleware/auth.global.ts
@@ -7,6 +7,10 @@ export default defineNuxtRouteMiddleware(async (to) => {
     if (to.path.startsWith("/dashboard")) {
       return navigateTo("/sign-in");
     }
+
+    if (to.path.startsWith("/posts")) {
+      return navigateTo("/sign-in");
+    }
   } else {
     if (to.path.startsWith("/sign-in")) {
       return navigateTo("/dashboard");
